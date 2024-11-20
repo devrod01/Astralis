bf320b678d316100d42fcf01b165aec5bb225291
Rodrigo Herrmann <66914016+devrod01@users.noreply.github.com>
2024-11-20 19:30:17 -0300

    Add files via upload

---
+++ Astralis v1.3/scripts/config_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _ready() -> void:
+	$CanvasLayer/VBoxContainer/Voltar.grab_focus()
+
+func _on_voltar_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_audio_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_video_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_controles_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_idiomas_pressed() -> void:
+	pass # Replace with function body.
