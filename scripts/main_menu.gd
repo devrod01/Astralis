1843f1339f9069ed4e9d15ac675100e4c38b5445
Ricardo_Augusto <89270938+FlorestNerd19@users.noreply.github.com>
2024-11-20 18:47:15 -0300

    Add files via upload

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,21 @@
 extends Control
 var video = false
+
+@onready var music_player = $MainMenuMusic
 func _ready() -> void:
 	$CanvasLayer/VBoxContainer/PlayButton.grab_focus()
+	# Inicia a música
+	music_player.play()
+	var tween = get_tree().create_tween()
+	# Anima o volume de -80 dB para 0 dB (volume normal)
+	tween.interpolate_value(
+		music_player, "volume_db", 
+		-80, 0, 
+		3,  # Duração em segundos
+		tween.TRANS_LINEAR
+	)
+	tween.play()
 	
-
 func _on_play_button_pressed() -> void:
 	if video == false:
 		get_tree().change_scene_to_file("res://main_intro.tscn")
@@ -13,7 +25,6 @@ func _on_play_button_pressed() -> void:
 
 func _on_config_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://config_menu.tscn")
-	#get_tree().change_scene_to_file("res://menu_config.tscn")
+	
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-	
